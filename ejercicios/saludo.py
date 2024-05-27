431be6fe2db93ea27765d93e8fb34f2b138d953f
lucassoriabusto <lucassoriabusto@hotmail.com>
2024-05-26 20:17:45 -0700

    Update

--- ejercicios/saludo.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def saludo():
-    nombre = input("Cómo te llamas?: ")
-    estado = input("¿Cómo estás?: ")
-    saludo = f"Hola {nombre}!!. Me alegro que estés {estado}"
-    print(saludo)
-
-saludo()
